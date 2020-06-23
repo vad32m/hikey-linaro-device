4f44d2ff989eb1e1713b5784296401bc0338442c
Vadym Mishchuk <superoriginalmailboxname@gmail.com>
2020-06-23 21:57:53 +0300

    Change path to kernel and device tree binaries
    Signed-off-by: Vadym Mishchuk <superoriginalmailboxname@gmail.com>

--- hikey960.mk
+++ hikey960.mk
@@ -1,8 +1,8 @@
 ifndef TARGET_KERNEL_USE
 TARGET_KERNEL_USE=4.9
 endif
-TARGET_PREBUILT_KERNEL := device/linaro/hikey-kernel/Image.gz-hikey960-$(TARGET_KERNEL_USE)
-TARGET_PREBUILT_DTB := device/linaro/hikey-kernel/hi3660-hikey960.dtb-$(TARGET_KERNEL_USE)
+TARGET_PREBUILT_KERNEL := out/target/product/hikey960/Image.gz-hikey960-$(TARGET_KERNEL_USE)
+TARGET_PREBUILT_DTB := out/target/product/hikey960/hi3660-hikey960.dtb-$(TARGET_KERNEL_USE)
 
 ifeq ($(TARGET_KERNEL_USE), 4.4)
   HIKEY_USE_LEGACY_TI_BLUETOOTH := true
